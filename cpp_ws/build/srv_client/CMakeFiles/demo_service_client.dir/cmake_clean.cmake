e3937aac8cad5e493c78bc453cda482df67a7fe0
Victory Chibuike <victorychibuike121@gmail.com>
2024-10-03 15:25:11 +0100

    Created a service client that uses a builtin interface

---
+++ cpp_ws/build/srv_client/CMakeFiles/demo_service_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_service_client.dir/src/demo_service_client.cpp.o"
+  "CMakeFiles/demo_service_client.dir/src/demo_service_client.cpp.o.d"
+  "demo_service_client"
+  "demo_service_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_service_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
